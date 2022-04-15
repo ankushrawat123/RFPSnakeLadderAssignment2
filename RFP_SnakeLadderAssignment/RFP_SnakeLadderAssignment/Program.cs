df7029cb11e6aabf6019de30f5a3728fae9d2b94
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-15 20:33:05 +0530

    uploading SingleplayerSnakeLadder file in SinglePlayerSnakeLadder branch

---
+++ RFP_SnakeLadderAssignment/RFP_SnakeLadderAssignment/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RFP_SnakeLadderAssignment;
+Console.WriteLine("Welcome to Snake and Ladder Program");
+SinglePlayer.SinglePlayermethod();
+  
